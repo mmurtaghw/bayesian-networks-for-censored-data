Corrected the testing accuracy calculation

ipwcBNs.py
@@ -11,52 +11,60 @@ from feature_engine.discretisation import EqualFrequencyDiscretiser
 
 
 
-invert_predictions = True
-paramLearnWeights = True
+invert_predictions = False
+paramLearnWeights = False
+dependent_variable = "fustat"
+factors_to_use = ["fustat",  "transplant", "reject", "surv"]
+
 data_file =pd.read_csv('bndata_inverse.csv')
+
 
 
 for index, row in data_file.iterrows():
     data_file['birth.dt'][index] = (row['birth.dt'][-2] + row['birth.dt'][-1])
     data_file['age'][index] = int(row['age'])
 
-birthdate_data = data_file['birth.dt'].to_numpy()
-birthdate_data = birthdate_data.reshape((len(birthdate_data),1))
-
 disc = EqualFrequencyDiscretiser(q=10, variables=['age'])
 disc.fit(data_file)
-
 new_binned_age_data = disc.transform(data_file)
 
 data_file['new_binned_age'] = 'e'
 data_file['new_binned_age'] = new_binned_age_data['age']
-print(new_binned_age_data.head(20))
 
 
-print(data_file[["new_binned_age", "age"]].head(30))
-
-
-
-newdata = data_file[["fustat", "new_binned_age","surgery", "transplant", "mismatch", "hla.a2"]]
+newdata = data_file[factors_to_use + ["weights"]]
 train, test, y_train, y_test = train_test_split(newdata, newdata, test_size=.90, random_state = 20)
 
-
-weights = data_file[["weights"]].to_numpy()
+weights = train[["weights"]].to_numpy()
 weights = np.reshape(weights, (np.product(weights.shape),))
+train = train[factors_to_use]
 
-print("The training data set is")
-print(train)
+position_of_dependent_variable = newdata.columns.get_loc(dependent_variable)
+test = test[factors_to_use]
+test_y = test[dependent_variable]
+test[dependent_variable] = None
 
-model = BayesianNetwork.from_samples(train)
+
+
+print("The shape of train is ",train.shape)
+print("The shape of test is ",test.shape)
+
+print("The shape of weights is", weights.shape)
+
+
+model = BayesianNetwork.from_samples(train, weights=weights)
 if paramLearnWeights == True:
-    model.fit(newdata,weights=weights, n_jobs = 1)
+    print("Parameter is learning is taking place but with weights this time")
+    model.fit(train, weights=weights, n_jobs = 1)
 
-test_y = test["surgery"]
+
 test = test.to_numpy()
-
 res = model.predict(test)
 
-position_of_dependent_variable = 0
+# print("The predicted values are for the test set ")
+# for prediction in res:
+#     print(prediction[0])
+
 
 predict = []
 k = np.shape(res)
@@ -71,11 +79,15 @@ if invert_predictions == True:
             predict[index] = 1
         else:
             predict[index] =0
-acc = accuracy_score(predict, test_y)
 
+
+
+acc = accuracy_score(predict, test_y)
 confusionMTRX = confusion_matrix(test_y,predict)
 print(acc)
 print(confusionMTRX)
+
+
 
 model.plot()
 plt.savefig('inverse_bn.png')

zupanBNs.py
@@ -10,11 +10,14 @@ from sklearn.preprocessing import KBinsDiscretizer
 from feature_engine.discretisation import EqualFrequencyDiscretiser
 
 
+
 invert_predictions = False
 paramLearnWeights = True
-predict = []
+dependent_variable = "transplant"
+factors_to_use = ["fustat",  "transplant", "reject", "outcome","Tstar"]
 
 data_file =pd.read_csv('bndata_zupan.csv')
+
 
 
 for index, row in data_file.iterrows():
@@ -23,39 +26,49 @@ for index, row in data_file.iterrows():
 
 disc = EqualFrequencyDiscretiser(q=10, variables=['age'])
 disc.fit(data_file)
-
 new_binned_age_data = disc.transform(data_file)
 
-data_file['new_binned_age'] = 'initialization Variable'
+data_file['new_binned_age'] = 'e'
 data_file['new_binned_age'] = new_binned_age_data['age']
-print(new_binned_age_data.head(20))
 
 
-print(data_file[["new_binned_age", "age"]].head(30))
+newdata = data_file[factors_to_use + ["KMW"]]
+train, test, y_train, y_test = train_test_split(newdata, newdata, test_size=.90, random_state = 20)
 
-
-
-newdata = data_file[["fustat", "new_binned_age","surgery", "transplant", "hla.a2","reject","Tstar","outcome"]]
-train, test, y_train, y_test = train_test_split(newdata, newdata, test_size=0.5, random_state = 20)
-
-
-weights = data_file[["KMW"]].to_numpy()
+weights = train[["KMW"]].to_numpy()
 weights = np.reshape(weights, (np.product(weights.shape),))
+train = train[factors_to_use]
+
+position_of_dependent_variable = newdata.columns.get_loc(dependent_variable)
+test = test[factors_to_use]
+test_y = test[dependent_variable]
+test[dependent_variable] = None
+
+
+
+print("The shape of train is ",train.shape)
+print("The shape of test is ",test.shape)
+
+print("The shape of weights is", weights.shape)
+
 
 model = BayesianNetwork.from_samples(train)
 if paramLearnWeights == True:
-    model.fit(newdata,weights=weights, n_jobs = 1)
+    print("Parameter is learning is taking place but with weights this time")
+    model.fit(train, weights=weights, n_jobs = 1)
 
-dependent_variable = "fustat"
-
-test_y = test[dependent_variable]
-position_of_dependent_variable = newdata.columns.get_loc(dependent_variable)
 
 test = test.to_numpy()
-
 res = model.predict(test)
 
+# print("The predicted values are for the test set ")
+# for prediction in res:
+#     print(prediction[0])
+
+
+predict = []
 k = np.shape(res)
+
 for i in range(k[0]):
     predict.append(res[i][position_of_dependent_variable])
 predict = np.array(predict).reshape(k[0],1)
@@ -67,23 +80,21 @@ if invert_predictions == True:
         else:
             predict[index] =0
 
+
+
 acc = accuracy_score(predict, test_y)
 confusionMTRX = confusion_matrix(test_y,predict)
-
-
-
 print(acc)
 print(confusionMTRX)
 
 
-print("The position of the depend variable is ", position_of_dependent_variable)
 
 model.plot()
 plt.savefig('zupan_bn.png')
 
 
 
-
-
-
 # print(model.score(data_file[["trt","sex", "death"]].to_numpy(), data_file[["rltime"]].to_numpy()))
+
+
+