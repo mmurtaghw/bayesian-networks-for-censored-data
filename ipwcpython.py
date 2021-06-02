7f20fc071eb1064a7b36d40b6bd6fbdebfdc11c4
Sherwin Mascarenhas <sherwinfarrell@gmail.com>
2021-06-02 08:59:11 +0100

    Added new accuracy measurement

--- ipwcpython.py
+++ ipwcpython.py
@@ -1,30 +1,57 @@
 import pandas as pd
 from pomegranate import *
 import numpy as np
-import pygraphviz 
+import matplotlib.pyplot as plt
+import pygraphviz
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+
 
 data_file =pd.read_csv('bndata.csv')
 print(data_file.head)
 
-newdata = data_file[["trt","sex", "death"]].to_numpy()
-# print(data_file[["weights"]])
-# print(newdata.head)
+newdata = data_file[["trt","sex", "death"]]
+train, test, y_train, y_test = train_test_split(newdata, newdata, test_size=.25)
+
 
 weights = data_file[["weights"]].to_numpy()
-print(weights.flatten())
-print(weights.shape)
-
 weights = np.reshape(weights, (np.product(weights.shape),))
-print(weights.shape)
 
-death = data_file[["death"]].to_numpy()
-death = np.reshape(death, (np.product(death.shape),))
-print("The shape of death is  ", death.shape)
+# death = train[["death"]].to_numpy()
+# death = np.reshape(death, (np.product(death.shape),))
+# print("The shape of death is  ", death.shape)
+print("The training data set is")
+print(train)
+
+model = BayesianNetwork.from_samples(train )
+# print(model.score(data_file[["trt","sex", "death"]].to_numpy(), data_file[["rltime"]].to_numpy()))
+# model.fit(newdata,weights=weights, n_jobs = 1)
+deathclass = test["death"]
+test = test.to_numpy()
+
+res = model.predict(test)
+
+predict = []
+k = np.shape(res)
+for i in range(k[0]):
+    predict.append(res[i][2])
+predict = np.array(predict).reshape(k[0],1)
 
 
-model = BayesianNetwork.from_samples(newdata, algorithm='exact',weights= weights )
-# print(model.score(data_file[["trt","sex"]].to_numpy(), death))
+acc = accuracy_score(predict, deathclass)
 
-print(model.predict([[1, 2, None]]))
+confusionMTRX = confusion_matrix(deathclass,predict)
+print(acc)
+print(confusionMTRX)
 
 model.plot()
+plt.savefig('bn.png')
+
+print(deathclass[:20])
+print(res[:20])
+
+
+
+
+
