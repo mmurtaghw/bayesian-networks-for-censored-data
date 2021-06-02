deed3edfae2bf4a17615fd32858703b6514e1664
Sherwin Mascarenhas <sherwinfarrell@gmail.com>
2021-06-02 14:08:19 +0100

    Removed cluter and modified the code

--- ipwcBNs.py
+++ ipwcBNs.py
@@ -11,9 +11,9 @@ from feature_engine.discretisation import EqualFrequencyDiscretiser
 
 
 
-
+invert_predictions = True
+paramLearnWeights = True
 data_file =pd.read_csv('bndata_inverse.csv')
-print(data_file.head)
 
 
 for index, row in data_file.iterrows():
@@ -22,12 +22,6 @@ for index, row in data_file.iterrows():
 
 birthdate_data = data_file['birth.dt'].to_numpy()
 birthdate_data = birthdate_data.reshape((len(birthdate_data),1))
-
-# kbins = KBinsDiscretizer(n_bins=10, encode='ordinal', strategy='uniform')
-# birthdate_bins = kbins.fit(birthdate_data)
-# newbineed_birthdates = birthdate_bins.transform(birthdate_data)
-
-# data_file['age_binned'] = newbineed_birthdates
 
 disc = EqualFrequencyDiscretiser(q=10, variables=['age'])
 disc.fit(data_file)
@@ -43,54 +37,52 @@ print(data_file[["new_binned_age", "age"]].head(30))
 
 
 
-newdata = data_file[["new_binned_age","surgery", "transplant", "mismatch", "hla.a2", "fustat"]]
-train, test, y_train, y_test = train_test_split(newdata, newdata, test_size=.25)
+newdata = data_file[["fustat", "new_binned_age","surgery", "transplant", "mismatch", "hla.a2"]]
+train, test, y_train, y_test = train_test_split(newdata, newdata, test_size=.90, random_state = 20)
 
 
 weights = data_file[["weights"]].to_numpy()
 weights = np.reshape(weights, (np.product(weights.shape),))
 
-# death = train[["death"]].to_numpy()
-# death = np.reshape(death, (np.product(death.shape),))
-# print("The shape of death is  ", death.shape)
 print("The training data set is")
 print(train)
 
 model = BayesianNetwork.from_samples(train)
-# print(model.score(data_file[["trt","sex", "death"]].to_numpy(), data_file[["rltime"]].to_numpy()))
-# model.fit(newdata,weights=weights, n_jobs = 1)
+if paramLearnWeights == True:
+    model.fit(newdata,weights=weights, n_jobs = 1)
 
-deathclass = test["fustat"]
+test_y = test["surgery"]
 test = test.to_numpy()
 
 res = model.predict(test)
 
+position_of_dependent_variable = 0
+
 predict = []
 k = np.shape(res)
+
 for i in range(k[0]):
-    predict.append(res[i][2])
+    predict.append(res[i][position_of_dependent_variable])
 predict = np.array(predict).reshape(k[0],1)
 
-invert = False
-if invert == True:
+if invert_predictions == True:
     for index, prediction in enumerate(predict): 
         if prediction == 0:
             predict[index] = 1
         else:
             predict[index] =0
-acc = accuracy_score(predict, deathclass)
+acc = accuracy_score(predict, test_y)
 
-confusionMTRX = confusion_matrix(deathclass,predict)
+confusionMTRX = confusion_matrix(test_y,predict)
 print(acc)
 print(confusionMTRX)
 
 model.plot()
-plt.savefig('bn.png')
-
-print(deathclass[:20])
-print(res[:20])
+plt.savefig('inverse_bn.png')
 
 
+
+# print(model.score(data_file[["trt","sex", "death"]].to_numpy(), data_file[["rltime"]].to_numpy()))
 
 
 
--- zupanBNs.py
+++ zupanBNs.py
@@ -10,31 +10,23 @@ from sklearn.preprocessing import KBinsDiscretizer
 from feature_engine.discretisation import EqualFrequencyDiscretiser
 
 
-
+invert_predictions = False
+paramLearnWeights = True
+predict = []
 
 data_file =pd.read_csv('bndata_zupan.csv')
-print(data_file.head)
 
 
 for index, row in data_file.iterrows():
     data_file['birth.dt'][index] = (row['birth.dt'][-2] + row['birth.dt'][-1])
     data_file['age'][index] = int(row['age'])
 
-birthdate_data = data_file['birth.dt'].to_numpy()
-birthdate_data = birthdate_data.reshape((len(birthdate_data),1))
-
-# kbins = KBinsDiscretizer(n_bins=10, encode='ordinal', strategy='uniform')
-# birthdate_bins = kbins.fit(birthdate_data)
-# newbineed_birthdates = birthdate_bins.transform(birthdate_data)
-
-# data_file['age_binned'] = newbineed_birthdates
-
 disc = EqualFrequencyDiscretiser(q=10, variables=['age'])
 disc.fit(data_file)
 
 new_binned_age_data = disc.transform(data_file)
 
-data_file['new_binned_age'] = 'e'
+data_file['new_binned_age'] = 'initialization Variable'
 data_file['new_binned_age'] = new_binned_age_data['age']
 print(new_binned_age_data.head(20))
 
@@ -43,54 +35,55 @@ print(data_file[["new_binned_age", "age"]].head(30))
 
 
 
-newdata = data_file[["new_binned_age","surgery", "transplant", "mismatch", "hla.a2", "fustat"]]
-train, test, y_train, y_test = train_test_split(newdata, newdata, test_size=.25)
+newdata = data_file[["fustat", "new_binned_age","surgery", "transplant", "hla.a2","reject","Tstar","outcome"]]
+train, test, y_train, y_test = train_test_split(newdata, newdata, test_size=0.5, random_state = 20)
 
 
 weights = data_file[["KMW"]].to_numpy()
 weights = np.reshape(weights, (np.product(weights.shape),))
 
-# death = train[["death"]].to_numpy()
-# death = np.reshape(death, (np.product(death.shape),))
-# print("The shape of death is  ", death.shape)
-print("The training data set is")
-print(train)
-
 model = BayesianNetwork.from_samples(train)
-# print(model.score(data_file[["trt","sex", "death"]].to_numpy(), data_file[["rltime"]].to_numpy()))
-# model.fit(newdata,weights=weights, n_jobs = 1)
+if paramLearnWeights == True:
+    model.fit(newdata,weights=weights, n_jobs = 1)
 
-deathclass = test["fustat"]
+dependent_variable = "fustat"
+
+test_y = test[dependent_variable]
+position_of_dependent_variable = newdata.columns.get_loc(dependent_variable)
+
 test = test.to_numpy()
 
 res = model.predict(test)
 
-predict = []
 k = np.shape(res)
 for i in range(k[0]):
-    predict.append(res[i][2])
+    predict.append(res[i][position_of_dependent_variable])
 predict = np.array(predict).reshape(k[0],1)
 
-invert = False
-if invert == True:
+if invert_predictions == True:
     for index, prediction in enumerate(predict): 
         if prediction == 0:
             predict[index] = 1
         else:
             predict[index] =0
-acc = accuracy_score(predict, deathclass)
 
-confusionMTRX = confusion_matrix(deathclass,predict)
+acc = accuracy_score(predict, test_y)
+confusionMTRX = confusion_matrix(test_y,predict)
+
+
+
 print(acc)
 print(confusionMTRX)
 
+
+print("The position of the depend variable is ", position_of_dependent_variable)
+
 model.plot()
-plt.savefig('bn.png')
-
-print(deathclass[:20])
-print(res[:20])
+plt.savefig('zupan_bn.png')
 
 
 
 
 
+
+# print(model.score(data_file[["trt","sex", "death"]].to_numpy(), data_file[["rltime"]].to_numpy()))
